8752f7d01c2bc7e08c5b95426278b5854f2c02ae
Benjamin Ejrup <31949825+wexr@users.noreply.github.com>
2018-03-19 19:04:18 +0100

    Update Kvittering.java
    PrintWriter hentet. Tjekker for eksisterende fil og sletter såfremt den allerede er lavet.
    Mangler input til .txt

--- src/retur/pant/Kvittering.java
+++ src/retur/pant/Kvittering.java
@@ -1,6 +1,26 @@
 package retur.pant;
 
 public class Kvittering {
+  
+  java.io.File file = new java.io.File("txt/kvittering.txt");
+        if (file.exists()) { //Tjekker om der allerede ligger en kvittering.
+            file.delete(); // Såfremt der allerede var en kvittering, slettes denne således ny kan laves til pant kunde.
+            System.exit(0); // Efter filen er slettet lukkes dette if loop.
+        }
+
+        try (
+                // Create a file
+                java.io.PrintWriter output = new java.io.PrintWriter(file);
+        ) {
+            /* Indsættelse af værdier til .txt
+            */
+            output.print("text");
+            // Måske arraylist?
+
+            System.out.println("Kvittering lavet");
+            output.close();
+
+        }
 
 
 }
