338ddf7921c5e0d00ce55a6327a0067412240c23
Benjamin Ejrup <31949825+wexr@users.noreply.github.com>
2018-03-19 19:10:19 +0100

    Update Kvittering.java
    Evt. løsning med arraylist til writer. Kan først tjekket når jeg kommer hjem fra work.

--- src/retur/pant/Kvittering.java
+++ src/retur/pant/Kvittering.java
@@ -14,8 +14,22 @@ public class Kvittering {
                 // Create a file
                 java.io.PrintWriter output = new java.io.PrintWriter(file);
         ) {
+          
             /* Indsættelse af værdier til .txt fil.
+            Kan ikke bruge output.print eller println da den kun tager imod "text".
+            Tænker derfor værdier skal smides ind i et arraylist, og så køre arraylisten igennem.
+            
+            Måske noget lign:
+            
+            ArrayList<Kvit> temp = printKvit(true);
+            int i, k;
+            for(i=0; i<temp.size(); i++){
+                for (k=0; k<temp.get(i).kvitData(true).size(); k++){
+                    output.print(temp.get(i).kvitData(true).get(k));
+                }
             */
+          
+          
             output.print("text");
             // Måske arraylist?
 
