73497077343f728f2d7451589d6b6143022e0ea7
Benjamin Ejrup <31949825+wexr@users.noreply.github.com>
2018-03-19 19:36:18 +0100

    Update Beregning.java

--- src/retur/pant/Beregning.java
+++ src/retur/pant/Beregning.java
@@ -1,6 +1,52 @@
 package retur.pant;
 
 public class Beregning {
+  
+  int     antalA = 0;
+  int     antalB = 0;
+  int     antalC = 0;
+  
+  int     totalAntalPantsat = antalA + antalB + antalC;
+  double  totalPenge = 0;
+  
+  PantVærdi pantVærdi = new PantVærdi();
+  
+  /*
+  Metode afsnit til pantsatning af dåser/flasker.
+  
+  */
+  void tilføjPant(EnumType typer){
+      if(typer == EnumType.pantA){
+          totalPenge += pantVærdi.getPantA();
+          antalA++;
+      }
+    
+    if(typer == EnumType.pantB){
+          totalPenge += pantVærdi.getPantB();
+          antalB++;
+      }
+    
+    if(typer == EnumType.pantC){
+          totalPenge += pantVærdi.getPantC();
+          antalB++;
+      }
+    
+    //Getters for samlet antal + penge for pant.
+    public int getAntalA() {
+        return antalA;
+    }
+
+    public int getAntalB() {
+        return antalB;
+    }
+
+    public int getAntalC() {
+        return antalC;
+    }
+
+    public double getTotalPenge(){
+        return totalPenge;
+    }
 
 
 
